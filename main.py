c3984c4391214e3093f456940b159f1d45aa15ad
Esteban <esteban125333@gmail.com>
2024-11-05 21:50:21 -0300

    use a .env file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from Database import Database
+import dotenv
+import os
 import pathlib
 
-if not pathlib.Path("database.db").exists():
+dotenv.load_dotenv()
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME")
+
+if not pathlib.Path(DATABASE_FILENAME).exists():
     raise FileNotFoundError("Database does not exist")
-database = Database("database.db")
+database = Database(DATABASE_FILENAME)
 print(database.__str__())
 database.bye()
