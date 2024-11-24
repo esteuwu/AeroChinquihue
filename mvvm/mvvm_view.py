8be41ce7899202af19fe5435da56439e386d93ff
Esteban <esteban125333@gmail.com>
2024-11-24 14:52:34 -0300

    implement identification verification algorithm

--- mvvm/mvvm_view.py
+++ mvvm/mvvm_view.py
@@ -10,6 +10,63 @@ class BaseWidget(QtWidgets.QWidget):
 
 class View(BaseWidget):
     class ClientWidget(BaseWidget):
+        def is_identification_valid(self):
+            identification = self.identification.text().replace('-', '').replace('.', '')
+            if identification.count('K') + identification.count('k') > 1 or len(identification) < 2:
+                return False
+            if not identification.replace('K', '').replace('k', '').isnumeric():
+                return False
+            buffer = 0
+            multiplier = 2
+            for character in identification[-2::-1]:
+                if multiplier > 7:
+                    multiplier = 2
+                buffer += int(character) * multiplier
+                multiplier += 1
+            buffer = 11 - buffer % 11
+            if buffer == 11:
+                return identification[-1::] == '0'
+            elif buffer == 10:
+                return identification[-1::] == 'K' or identification[-1::] == 'k'
+            else:
+                return identification[-1::] == str(buffer)
+
+        def handle_flight_button(self):
+            # Show airplane widget
+            self.airplaneLabel.show()
+            self.airplane.show()
+            # Show date and time pickers
+            self.dateLabel.show()
+            self.date.show()
+            self.timeLabel.show()
+            self.time.show()
+            # Hide weight widgets
+            self.weightLabel.hide()
+            self.weight.hide()
+            # Show seats widgets
+            self.seatsLabel.show()
+            self.seats.show()
+
+        def handle_freight_button(self):
+            # Hide airplane widget
+            self.airplaneLabel.hide()
+            self.airplane.hide()
+            # Hide date and time pickers
+            self.dateLabel.hide()
+            self.date.hide()
+            self.timeLabel.hide()
+            self.time.hide()
+            # Hide seats widgets
+            self.seatsLabel.hide()
+            self.seats.hide()
+            # Show weight widgets
+            self.weightLabel.show()
+            self.weight.show()
+
+        def handle_ok_button(self):
+            if not self.is_identification_valid():
+                QtWidgets.QMessageBox.warning(self, "Advertencia", "El RUT ingresado es inválido.")
+
         def __init__(self, viewmodel):
             super().__init__()
             self.viewModel = viewmodel
@@ -24,11 +81,12 @@ class View(BaseWidget):
             self.buttonGroupLayout = QtWidgets.QHBoxLayout()
             # Flight button
             self.flightButton = QtWidgets.QRadioButton("Vuelo")
-            self.flightButton.setChecked(True)
+            self.flightButton.clicked.connect(self.handle_flight_button)
             self.buttonGroup.addButton(self.flightButton)
             self.buttonGroupLayout.addWidget(self.flightButton)
             # Freight button
             self.freightButton = QtWidgets.QRadioButton("Encomienda")
+            self.freightButton.clicked.connect(self.handle_freight_button)
             self.buttonGroup.addButton(self.freightButton)
             self.buttonGroupLayout.addWidget(self.freightButton)
             # Add button group layout to main layout
@@ -49,7 +107,8 @@ class View(BaseWidget):
             self.destination.addItems(self.viewModel.get_destinations())
             self.layout.addWidget(self.destination)
             # Airplane list
-            self.layout.addWidget(QtWidgets.QLabel("Avión"))
+            self.airplaneLabel = QtWidgets.QLabel("Avión")
+            self.layout.addWidget(self.airplaneLabel)
             self.airplane = QtWidgets.QComboBox()
             self.airplane.addItems(self.viewModel.get_airplanes())
             self.layout.addWidget(self.airplane)
@@ -57,32 +116,44 @@ class View(BaseWidget):
             self.dateTimeLayout = QtWidgets.QHBoxLayout()
             # Vertical layout for date picker
             self.dateLayout = QtWidgets.QVBoxLayout()
-            self.dateLayout.addWidget(QtWidgets.QLabel("Fecha de ida"))
+            self.dateLabel = QtWidgets.QLabel("Fecha de ida")
+            self.dateLayout.addWidget(self.dateLabel)
             self.date = QtWidgets.QCalendarWidget()
             self.date.setMinimumDate(QtCore.QDate.currentDate())
             self.dateLayout.addWidget(self.date)
             self.dateTimeLayout.addLayout(self.dateLayout)
             # Vertical layout for time picker
             self.timeLayout = QtWidgets.QVBoxLayout()
-            self.timeLayout.addWidget(QtWidgets.QLabel("Hora de ida"))
+            self.timeLabel = QtWidgets.QLabel("Hora de ida")
+            self.timeLayout.addWidget(self.timeLabel)
             self.time = QtWidgets.QListWidget()
             self.timeLayout.addWidget(self.time)
             self.dateTimeLayout.addLayout(self.timeLayout)
             # Add date and time layout to main layout
             self.layout.addLayout(self.dateTimeLayout)
             # Seats
-            self.layout.addWidget(QtWidgets.QLabel("Asientos"))
+            self.seatsLabel = QtWidgets.QLabel("Asientos")
+            self.layout.addWidget(self.seatsLabel)
             self.seats = QtWidgets.QLineEdit()
             self.seats.setPlaceholderText("ej. 1")
             self.layout.addWidget(self.seats)
+            # Weight
+            self.weightLabel = QtWidgets.QLabel("Peso (en kilogramos)")
+            self.layout.addWidget(self.weightLabel)
+            self.weight = QtWidgets.QLineEdit()
+            self.weight.setPlaceholderText("ej. 10")
+            self.layout.addWidget(self.weight)
+            # Set default service
+            self.flightButton.click()
             # Payment method
             self.layout.addWidget(QtWidgets.QLabel("Medio de pago"))
             self.paymentMethod = QtWidgets.QComboBox()
             self.paymentMethod.addItems(self.viewModel.get_payment_methods())
             self.layout.addWidget(self.paymentMethod)
-            # Confirm button
-            self.confirmButton = QtWidgets.QPushButton("Confirmar")
-            self.layout.addWidget(self.confirmButton)
+            # OK button
+            self.okButton = QtWidgets.QPushButton("OK")
+            self.okButton.clicked.connect(self.handle_ok_button)
+            self.layout.addWidget(self.okButton)
 
     class ManagerWidget(BaseWidget):
         def __init__(self, viewmodel):
