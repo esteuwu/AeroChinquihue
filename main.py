8f0477e95b4e9c50fdcbdb2eab2dadd726a02502
Esteban <esteban125333@gmail.com>
2024-11-27 16:21:47 -0300

    Revert "Merge branch 'master' of https://github.com/esteuwu/AeroChinquihue"
    This reverts commit cb507b8971d270c28d10608e64044488951fa53d, reversing
    changes made to 86a1bef67e3fca86dde352f4ca7deadc2f5363b6.

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ if __name__ == "__main__":
     # Driver code
     app = PySide6.QtWidgets.QApplication(sys.argv)
     if not pathlib.Path(DATABASE_FILENAME).exists():
-<<<<<<< HEAD
-        raise FileNotFoundError("Database does not exist")
-    model = mvvm_model.Model(DATABASE_FILENAME)
-    viewModel = mvvm_view_model.ViewModel(model)
-    view = mvvm_view.View(viewpip install PySide6
-
-=======
         PySide6.QtWidgets.QMessageBox.critical(PySide6.QtWidgets.QWidget(), "Error crítico", "La base de datos no "
                                                                                              "existe.")
         sys.exit(1)
@@ -28,4 +21,3 @@ if __name__ == "__main__":
     view = mvvm.View(viewModel)
     view.show()
     sys.exit(app.exec())
->>>>>>> 86a1bef67e3fca86dde352f4ca7deadc2f5363b6
