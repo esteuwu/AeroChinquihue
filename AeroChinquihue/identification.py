4b0c6e1e1e0ac9cb551408077b2a5fddd8180dc2
Esteban <esteban125333@gmail.com>
2024-12-02 20:51:51 -0300

    modify identification

--- AeroChinquihue/identification.py
+++ AeroChinquihue/identification.py
@@ -1,6 +1,6 @@
 class Identification:
-    def __init__(self, identification):
-        if not self.is_identification_valid(identification):
+    def __init__(self, identification: str):
+        if not self._is_identification_valid(identification):
             raise ValueError("Invalid identification")
         self.identification = identification
 
@@ -8,20 +8,20 @@ class Identification:
         return int(self.identification.replace('-', '').replace('.', '')[:-1])
 
     @staticmethod
-    def is_identification_valid(identification):
+    def _is_identification_valid(identification: str):
         identification = identification.replace('-', '').replace('.', '')
         if identification.count('K') + identification.count('k') > 1 or len(identification) < 2 or not identification.replace('K', '').replace('k', '').isnumeric():
             return False
-        buffer = 0
         multiplier = 2
+        result = 0
         for character in identification[-2::-1]:
             if multiplier > 7:
                 multiplier = 2
-            buffer += int(character) * multiplier
+            result += int(character) * multiplier
             multiplier += 1
-        buffer = 11 - buffer % 11
-        if buffer == 10:
+        result = 11 - result % 11
+        if result == 10:
             return identification[-1] == 'K' or identification[-1] == 'k'
-        if buffer == 11:
+        if result == 11:
             return identification[-1] == '0'
-        return identification[-1] == str(buffer)
+        return identification[-1] == str(result)
