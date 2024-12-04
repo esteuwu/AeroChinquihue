c2f40c1c5f3cf3edc9d1c6908d75910b4488e54f
Esteban <esteban125333@gmail.com>
2024-12-04 19:40:04 -0300

    add missing feature

--- package/viewmodel.py
+++ package/viewmodel.py
@@ -12,10 +12,10 @@ from .model import Model
 class ViewModel:
     """Class to interact between the Model and View classes."""
     def add_flight(self, values: tuple):
-        self.model.add_flight((str(uuid.uuid4()),) + values + (0, int(time.time())))
+        self.model.add_flight((str(uuid.uuid4()),) + values + (self.get_prices(values[2])[0] * values[5], int(time.time())))
 
     def add_freight(self, values: tuple):
-        self.model.add_freight((str(uuid.uuid4()),) + values + (0, int(time.time())))
+        self.model.add_freight((str(uuid.uuid4()),) + values + (self.get_prices(values[2])[1] * values[3], int(time.time())))
 
     def delete_flight(self, flight_uuid: str):
         self.model.delete_flight((flight_uuid,))
